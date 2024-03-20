f0a14abc90d5b50ca0007ad03ec8379470cdd323
Bernardo <bgbaine@gmail.com>
2024-03-20 20:42:48 -0300

    Create filme.js

---
+++ 1y/1s/lp/programas/aula3/filme.js
@@ -0,0 +1,6 @@
+// Lê o título e duração de um filme em minutos e informa a duração em horas e minutos.
+
+const prompt = require("prompt-sync")()
+
+const filme = prompt("Filme: ")
+const duracao = Number(prompt("Duração (min): "))
