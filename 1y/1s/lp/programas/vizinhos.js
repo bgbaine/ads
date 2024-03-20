5c928c948924606239e2f80249a3e6f5142b8197
Bernardo <bgbaine@gmail.com>
2024-03-20 20:28:21 -0300

    Update vizinhos.js

--- 1y/1s/lp/programas/vizinhos.js
+++ 1y/1s/lp/programas/vizinhos.js
@@ -3,4 +3,5 @@
 const prompt = require("prompt-sync")()
 
 const numero = Number(prompt("Número: "))
+
 console.log(`O número ${numero - 1} antecede o número ${numero}, enquanto ${numero + 1} o sucede`)
