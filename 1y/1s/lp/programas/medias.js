71a65591bb73895e95a8f67d779942decd0112b9
Bernardo <bgbaine@gmail.com>
2024-03-20 20:27:58 -0300

    Update medias.js

--- 1y/1s/lp/programas/medias.js
+++ 1y/1s/lp/programas/medias.js
@@ -4,4 +4,5 @@ const prompt = require("prompt-sync")()
 
 const nota1 = Number(prompt("Nota 1: "))
 const nota2 = Number(prompt("Nota 2: "))
+
 console.log(`A média das notas é ${((nota1 + nota2) / 2).toFixed(2)}`)
