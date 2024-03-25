7818b522f154c9b99f766ffe6c9d3fec48710624
Bernardo <bgbaine@gmail.com>
2024-03-25 17:38:24 -0300

    Delete 1s directory

--- 1s/lp/aula3/aplicacoes/filme.js
+++
@@ -1,9 +0,0 @@
-// Lê o título e duração de um filme em minutos e informa a duração em horas e minutos.
-
-const prompt = require("prompt-sync")()
-
-const filme = prompt("Filme: ")
-const duracao = Number(prompt("Duração (min): "))
-
-console.log(`O filme ${filme}`)
-console.log(`Tem a duração de ${Math.floor(duracao / 60)} horas e ${duracao % 60} minutos.`)
