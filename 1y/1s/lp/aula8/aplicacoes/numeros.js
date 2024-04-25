0645135aa81037d3f8a9f1ab1342634ec92c5c1f
Bernardo <bgbaine@gmail.com>
2024-04-24 22:31:07 -0300

    Add files via upload

--- 1y/1s/lp/aula8/aplicacoes/numeros.js
+++ 1y/1s/lp/aula8/aplicacoes/numeros.js
@@ -1,4 +1,30 @@
-// Lê o preço e o modelo de 'n' veículos e exibe a lista dos veículos, bem como o preço médio dos veículos
+// Lê 'n' números (até ser digitado zero) e exibe os números em grupos pares ou ímpares
 
 const prompt = require("prompt-sync")()
 
+const numeros = [] 
+
+let numero
+do {
+    numero = Number(prompt("Número: "))
+    
+    if (numero != 0) {
+        numeros.push(numero)
+    }
+} while (numero != 0)
+
+console.log("\nPares da Lista")
+console.log("-".repeat(30))
+for (let i = 0; i < numeros.length; i++) {
+    if ((numeros[i] % 2) == 0) {
+        console.log(numeros[i])
+    }
+}
+
+console.log("\nÍmpares da Lista")
+console.log("-".repeat(30))
+for (let i = 0; i < numeros.length; i++) {
+    if ((numeros[i] % 2) != 0) {
+        console.log(numeros[i])
+    }
+}
