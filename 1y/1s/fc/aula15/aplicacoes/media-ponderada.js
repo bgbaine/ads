6da493f0a217682ae9a5ce979686f7dfc1375298
Bernardo <bgbaine@gmail.com>
2024-06-25 19:26:03 -0300

    refactor: update media-ponderada.js

--- 1y/1s/fc/aula15/aplicacoes/media-ponderada.js
+++ 1y/1s/fc/aula15/aplicacoes/media-ponderada.js
@@ -6,13 +6,20 @@ const prompt = require("prompt-sync")();
 const nota1 = Number(prompt("1ª Nota: "));
 const nota2 = Number(prompt("2ª Nota: "));
 const nota3 = Number(prompt("3ª Nota: "));
+let media = 0;
 
 if (nota1 > nota2 && nota1 > nota3) {
-    console.log(calcularMedia(nota1, [nota2, nota3]));
+    media = calcularMedia(nota1, [nota2, nota3]);
 } else if (nota2 > nota1 && nota2 > nota3) {
-    console.log(calcularMedia(nota2, [nota1, nota3]));
+    media = calcularMedia(nota2, [nota1, nota3]);
 } else {
-    console.log(calcularMedia(nota3, [nota1, nota2]));
+    media = calcularMedia(nota3, [nota1, nota2]);
+}
+
+if (media >= 7) {
+    console.log("APROVADO - Média: " + media);
+} else {
+    console.log("REPROVADO - Média: " + media);
 }
 
 function calcularMedia(maior, restante) {
