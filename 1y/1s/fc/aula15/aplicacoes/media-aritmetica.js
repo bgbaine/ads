97aefd2f63dceae4cb9304f82caa8278362e4074
Bernardo <bgbaine@gmail.com>
2024-06-25 19:20:46 -0300

    refactor: update media-aritmetica.js

--- 1y/1s/fc/aula15/aplicacoes/media-aritmetica.js
+++ 1y/1s/fc/aula15/aplicacoes/media-aritmetica.js
@@ -7,4 +7,4 @@ const numero1 = Number(prompt("1º Número: "));
 const numero2 = Number(prompt("2º Número: "));
 const numero3 = Number(prompt("3º Número: "));
 
-console.log("A média aritmética dos números informados é ", (numero1 + numero2 + numero3) / 3);
+console.log("A média aritmética dos números informados é " + (numero1 + numero2 + numero3) / 3);
