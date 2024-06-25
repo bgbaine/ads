195b005ba59b14c9a74111007f44f6b94fd9e557
Bernardo <bgbaine@gmail.com>
2024-06-25 19:54:48 -0300

    refactor: update intervalo.js

--- 1y/1s/fc/aula15/aplicacoes/intervalo.js
+++ 1y/1s/fc/aula15/aplicacoes/intervalo.js
@@ -11,7 +11,7 @@ if ((numero > 5 && numero < 10) || numero <= 2 || numero >= 20) {
     console.log("O número não está dentro do intervalo determinado");
 }
 
-if ((numero <= 5 || numero >= 10) && numero > 2 && numero < 20) {
+if ((numero <= 5 || numero >= 10) && (numero > 2 && numero < 20)) {
     console.log("O número não está dentro do intervalo determinado");
 } else {
     console.log("O número está dentro do intervalo determinado");
