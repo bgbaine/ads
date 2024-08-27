f436d6ba49ab6f9c1e165c889ff4b87d8014f803
Bernardo <bgbaine@gmail.com>
2024-08-27 15:43:33 -0300

    chore: update remote repo

--- 1y/1s/lp/aula11/aplicacoes/asteriscos.js
+++
@@ -1,9 +0,0 @@
-// Lê um número e um nome e exibe o nome entre '*', onde o número indica a quantidade de '*' antes e depois do nome
-
-const prompt = require("prompt-sync")()
-
-
-const nome = prompt("Nome: ")
-const numero = Number(prompt("Número: "))
-
-console.log(`${"*".repeat(numero)} ${nome} ${"*".repeat(numero)}`)
--- 1y/1s/lp/aula3/aplicacoes/distancia.js
+++
@@ -1,7 +0,0 @@
-// Lê a distância percorrida por um ciclista em metros e exibe o equivalente em km e metros.
-
-const prompt = require("prompt-sync")()
-
-const distancia = prompt("Distância Percorrida (m): ")
-
-console.log(`Equivale a ${Math.floor(distancia / 1000)}km e ${distancia % 1000}m`)
