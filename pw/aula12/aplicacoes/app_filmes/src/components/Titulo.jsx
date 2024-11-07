9743eddf12cb1b7ebd93b740bb8921b2b5707f9b
Bernardo <bgbaine@gmail.com>
2024-11-06 21:27:42 -0300

    chore: update remote repo

---
+++ pw/aula12/aplicacoes/app_filmes/src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+export function Titulo() {
+  return (
+    <header>
+      <img src="./pipoca.png" alt="Cinema e Pipoca" />
+      <div>
+        <h1>App Controle de Filmes</h1>
+        <h2>Cadastro e Avaliação Pessoal de Filmes</h2>
+      </div>
+    </header>
+  )
+}
