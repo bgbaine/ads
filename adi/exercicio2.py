773ec6876db817da30192f52b0db29b54de849d8
Bernardo Carvalho <bgbaine@gmail.com>
2025-03-20 21:23:02 -0300

    chore: update remote repo

--- adi/exercicio2.py
+++ adi/exercicio2.py
@@ -2,11 +2,21 @@ def main():
     numero: int = int(input("Número: "))
     divisores: list = []
 
-    for i in range(numero, 1, -1):
+    for i in range(numero // 2, 0, -1):
         if numero % i == 0:
             divisores.append(i)
 
-    print(divisores)
+    print(f"Divisores do {numero}: ", end="")
+    divisores.reverse()
+
+    print(", ".join(map(str, divisores)))
+
+    soma_divisores: int = sum(divisores)
+    print(f"Soma dos divisores: {soma_divisores}")
+
+    print(f"Portanto, {numero} ", end="")
+    print("não é ", end="") if soma_divisores != numero else print("é ", end="")
+    print("um número perfeito")
 
 
 if __name__ == "__main__":
