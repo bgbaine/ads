7c68fe3f60e46fc589b9e0cb343bd440a3b0eb21
Bernardo Carvalho <bgbaine@gmail.com>
2025-08-19 21:33:07 -0300

    chore: update remote repo

---
+++ adii/aula3/exercicios/alg.py
@@ -0,0 +1,16 @@
+# total de casos
+# casos gerados
+
+l: list[str] = [
+    "Usuårio é maior de idade?",
+    "Tem conta ativa?",
+    "Possui limite de credito",
+    "E cliente premium",
+]
+
+
+def get_combinations(lista: list[str]) -> None:
+    pass
+
+
+print(get_combinations(l))
